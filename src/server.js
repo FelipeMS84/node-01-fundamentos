ccf5881633f107b877380ecf221c48017e63560a
FelipeMS84 <felipe.manoel.silva@protonmail.com>
2023-06-03 16:59:25 -0300

    Estrutura da aplicação

--- src/server.js
+++ src/server.js
@@ -1,7 +1,34 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((req, res)=> {
-    res.end("Hello Ignite")
+
+
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users') {
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+        
+    }
+
+    if(method === 'POST' && url === '/users') {
+
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+
+        return res.writeHead(201).end();
+    }
+
+
+
+
+    return res.end("Hello Ignite")
 });
 
 server.listen(3333);
